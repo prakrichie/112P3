61d8a718a29b4189225a6f3503aefefdb9643f31
prakrichie <37090745+prakrichie@users.noreply.github.com>
2018-10-19 13:39:40 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+
+
+//function setup(){
+//createCanvas(1024, 720);
+ //width & height
+
+//background(255);
+
+
+//}
+
+
+
+
+//function draw(){
+    //noStroke();
+    //fill(200, 128, 224);
+    //rect(15, 10, 100, 100);
+
+    //}
+
+
+
+
+
+
+
